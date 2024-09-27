21e580b09b1b63fad85092f23161a600447be160
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-09-27 14:18:34 +0200

    Feat: Implement basic formatter and add  to body text to remove unecessary whitespace

--- manual-test.js
+++ manual-test.js
@@ -1,11 +1,21 @@
+import fs from 'node:fs'
 import { CodeRepoReadmeParser } from './lib/CodeRepoReadmeParser.js'
+// import { parseMarkdown } from './index.js'
+
+// console.log(parseMarkdown('./aux/test-readmes/README_airbnb.md'))
+
 
 const context = new CodeRepoReadmeParser('./aux/test-readmes/README_airbnb.md')
 
-const titleAndDescription = context.getTitleAndDescription()
-const installation = context.getInstallationInstructions()
-const usageExamples = context.getUsageExamples()
-const contributionGuidelines = context.getContributionGuidelines()
-const licenseInfo = context.getLicenseInfo()
 
-console.log(titleAndDescription, installation, usageExamples, contributionGuidelines, licenseInfo)
+const titleAndDescription = context.getTitleAndDescription()
+// const installation = context.getInstallationInstructions()
+// const usageExamples = context.getUsageExamples()
+//const contributionGuidelines = context.getContributionGuidelines()
+// const licenseInfo = context.getLicenseInfo()
+
+// console.log(titleAndDescription)
+
+// installation, usageExamples, contributionGuidelines, licenseInfo
+
+fs.writeFileSync('./aux/logs/CRRP-airbnb.log', titleAndDescription.description)
