cfe2555a41204429213be32f73b49ce4902804fe
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-09-30 13:19:30 +0200

    Chore: remove temp manual-test file

--- manual-test.js
+++ manual-test.js
@@ -1,21 +1,27 @@
-import fs from 'node:fs'
-import { CodeRepoReadmeParser } from './lib/CodeRepoReadmeParser.js'
+// import fs from 'node:fs'
+import { CodeRepoReadmeParser } from './dist/lib/CodeRepoReadmeParser.js'
 // import { parseMarkdown } from './index.js'
 
-// console.log(parseMarkdown('./aux/test-readmes/README_airbnb.md'))
+// const allSections = parseMarkdown('./aux/test-readmes/README_bics.md')
 
 
-const context = new CodeRepoReadmeParser('./aux/test-readmes/README_airbnb.md')
-
-
-const titleAndDescription = context.getTitleAndDescription()
 // const installation = context.getInstallationInstructions()
 // const usageExamples = context.getUsageExamples()
-//const contributionGuidelines = context.getContributionGuidelines()
+// const contributionGuidelines = context.getContributionGuidelines()
 // const licenseInfo = context.getLicenseInfo()
 
-// console.log(titleAndDescription)
-
 // installation, usageExamples, contributionGuidelines, licenseInfo
-
-fs.writeFileSync('./aux/logs/CRRP-airbnb.log', titleAndDescription.description)
+// fs.writeFileSync('./aux/logs/CRRP-bics_title.log', titleAndDescription.title)
+// fs.writeFileSync('./aux/logs/CRRP-bics_Description.log', titleAndDescription.description)
+try {
+  // const context = new CodeRepoReadmeParser('./aux/test-readmes/README_airbnb.md')
+  const context = new CodeRepoReadmeParser('./README.md')
+  // console.log('Test get sections:', context.sections)
+  // console.log('Test get titleAndDescription:', context.titleAndDescription)
+  // console.log('Test get title:', context.title)
+  // console.log('Test getHeadingLevels(1):', context.getHeadingLevels(1))
+  console.log('Test findSectionByKeywords:', context.getSectionsWithHeading('install'))
+  // console.log('Test get usageExamples:', context.usageExamples)
+} catch (err) {
+  console.error('Error:', err.message)
+}
