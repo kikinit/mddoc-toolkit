0640dd8011c410b07a9daf3024d537a22bb68df9
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-10-02 21:07:48 +0200

    Refactor: Update index.ts with imports/exports to work as proxy

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
-// Imports
-import { MarkdownParser } from './lib/MarkdownParser.js'
+// Imports of parser and contexts
+import { MarkdownParser, Section } from './lib/MarkdownParser'
+import { RepoReadmeProcessor} from './lib/RepoReadmeProcessor'
+import { NpmReadmeProcessor} from './lib/NpmReadmeProcessor'
+import { ChangelogProcessor } from './lib/ChangelogProcessor'
 
 // Export the core classes.
-export { MarkdownParser }
+export {
+  MarkdownParser,
+  RepoReadmeProcessor,
+  NpmReadmeProcessor,
+  ChangelogProcessor
+}
+
 
 // Export a convenience function for quick parsing.
-export function parseMarkdown(filePath) {
+export function parseMarkdown(filePath: string): Section[] {
   const parser = new MarkdownParser(filePath)
-  return parser.getSections()
+  return parser.sections
 }
